295674dfcf10ffc5081d238268673852275ed4d8
Deepak Sharma <stdeepak22@gmail.com>
2025-02-13 23:38:21 +0530

    removed the duplicate provider from App component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { Link, BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Login } from './features/login/components/login';
 import { Counter } from './features/counter/components/counter';
-import { Provider, useDispatch, useSelector } from 'react-redux';
-import store from './store';
+import { useDispatch, useSelector } from 'react-redux';
 import { selectIsLoggedIn, selectUserName } from './features/globalState/globalStateSelector';
 import { performLogout } from './features/globalState/globalStateSlice';
 import { HomePage } from './features/home/home-page';
@@ -40,27 +39,25 @@ function NavBar() {
 function App() {
     const isLoggedIn = useSelector(selectIsLoggedIn);
     return (
-        <Provider store={store}>
-            <Router>
-                <LoginStatus />
-                <NavBar />
-                <Switch>
-                    <Route path="/counter">
-                        <Counter />
+        <Router>
+            <LoginStatus />
+            <NavBar />
+            <Switch>
+                <Route path="/counter">
+                    <Counter />
+                </Route>
+                {isLoggedIn ? <Route path="/secret-page">
+                    <SecretPage />
+                </Route>
+                    : <Route path="/login">
+                        <Login />
                     </Route>
-                    {isLoggedIn ? <Route path="/secret-page">
-                        <SecretPage />
-                    </Route>
-                        : <Route path="/login">
-                            <Login />
-                        </Route>
-                    }
-                    <Route default path="/">
-                        <HomePage />
-                    </Route>
-                </Switch>
-            </Router>
-        </Provider>
+                }
+                <Route default path="/">
+                    <HomePage />
+                </Route>
+            </Switch>
+        </Router>
     );
 }
 
